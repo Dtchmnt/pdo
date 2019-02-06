675c98681a5d27d473dd00149320e0f26508e960
Dtchmnt <dtchmnt.ishim@gmail.com>
2019-02-06 19:32:33 +0700

    Make table

--- index.php
+++ index.php
@@ -9,11 +9,30 @@ try {
 
 try {
     $getAll = $db->getAll("SELECT card.id, card.name, card.category_id, category.id, category.name as category FROM card, category WHERE  card.category_id = category.id");
-    foreach ($getAll as $row) {
-        echo $row['id'] . "<br />\n";
-        echo $row['name'] . "<br />\n";
-        echo $row['category'] . "<br />\n";
-    }
 } catch (Exception $e) {
+
 }
+?>
+
+<?php foreach ($getAll as $row): ?>
+    <table border="1">
+        <tr>
+            <td>
+                <b>ID</b>
+            </td>
+            <td>
+                <b>Name</b>
+            </td>
+            <td>
+                <b>Category</b>
+            </td>
+        <tr>
+            <td> <? echo $row['id'] ?> </td>
+            <td> <? echo $row['name'] ?> </td>
+            <td> <? echo $row['category'] ?> </td>
+        </tr>
+    </table>
+
+<?php endforeach;
+
 $db->disconnect();
